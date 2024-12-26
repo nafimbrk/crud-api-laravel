2587343c6975c4b6f576cf350f2326bccb6a7088
nafimbrk <nafimubarok2005@gmail.com>
2024-12-26 16:49:36 +0700

    hapus kode gk penting

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\Api1_tokobuku;
 use App\Http\Controllers\MatakuliahController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,7 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('/api1',Api1_tokobuku::class);
-
 Route::resource('/mk',MatakuliahController::class);
-
